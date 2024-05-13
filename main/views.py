3a2bf3ae66647fb565fae504148944054201829e
ApplePie <novikov1089@gmail.com>
2024-05-14 01:47:03 +0500

    get main page

--- main/views.py
+++ main/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-
-    context = {"title": "Home", "content": "Content"}
+    context = {"title": "Главная", "content": "Магазин украшений N.V.E Jewerly"}
 
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About")
+    context = {
+        "title": "Главная",
+        "content": "Магазин украшений N.V.E Jewerly",
+        "text_on_page": "Почему этот магазим",
+    }
+
+    return render(request, "main/about.html", context)
